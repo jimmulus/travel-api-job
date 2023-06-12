e9aa9215221710a07ac22abb1b10ff69012655a7
jimmulus <jimmulus@gmail.com>
2023-06-12 16:23:47 +0200

    made slug unique

--- database/migrations/2023_06_12_084934_create_travel_table.php
+++ database/migrations/2023_06_12_084934_create_travel_table.php
@@ -13,7 +13,7 @@ return new class () extends Migration {
         Schema::create('travel', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->boolean('is_public');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->string('name');
             $table->string('description');
             $table->integer('number_of_days');
